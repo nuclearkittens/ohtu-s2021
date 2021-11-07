2a2dada976f8f27ccad85007884d31a0e0916f60
meri saynatkari <meri.saynatkari@gmail.com>
2021-11-07 16:42:24 +0200

    ex 15

--- wk1/nhl-statistics-1/src/index.py
+++ wk1/nhl-statistics-1/src/index.py
@@ -1,8 +1,9 @@
 from statistics import Statistics
-
+from player_reader import PlayerReader
 
 def main():
-    stats = Statistics()
+    reader = PlayerReader()
+    stats = Statistics(reader)
     philadelphia_flyers_players = stats.team("PHI")
     top_scorers = stats.top_scorers(10)
 
@@ -13,7 +14,6 @@ def main():
     print("Top scorers:")
     for player in top_scorers:
         print(player)
-
 
 if __name__ == "__main__":
     main()
--- wk1/nhl-statistics-1/src/statistics.py
+++ wk1/nhl-statistics-1/src/statistics.py
@@ -1,15 +1,10 @@
-from player_reader import PlayerReader
-
-
 def sort_by_points(player):
     return player.points
 
-
 class Statistics:
-    def __init__(self):
-        reader = PlayerReader()
-
-        self._players = reader.get_players()
+    def __init__(self, plr_reader):
+        self._reader = plr_reader
+        self._players = self._reader.get_players()
 
     def search(self, name):
         for player in self._players:
