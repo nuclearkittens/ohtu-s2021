c6b0917717a024cd671f8a2f6489e304215d8433
meri saynatkari <meri.saynatkari@gmail.com>
2021-11-15 00:12:09 +0200

    wk2 ex12

--- wk2/verkkokauppa-1/src/kauppa.py
+++ wk2/verkkokauppa-1/src/kauppa.py
@@ -1,7 +1,15 @@
 from ostoskori import Ostoskori
+from pankki import pankki as default_pankki
+from varasto import varasto as default_varasto
+from viitegeneraattori import viitegeneraattori as default_viitegen
+
 
 class Kauppa:
-    def __init__(self, varasto, pankki, viitegeneraattori):
+    def __init__(
+        self, varasto=default_varasto,
+        pankki=default_pankki,
+        viitegeneraattori=default_viitegen
+    ):
         self._varasto = varasto
         self._pankki = pankki
         self._viitegeneraattori = viitegeneraattori
