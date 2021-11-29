1b46bf24fc7f55f49c6920c7078158cb8805a409
meri saynatkari <meri.saynatkari@gmail.com>
2021-11-29 11:07:16 +0200

    wk4 ex5 pt4: tests written and passed (kaksi tavaraa määrä oikein)

--- wk4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ wk4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,7 +5,7 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
-        self.tuotteet = [Tuote('vuustonaxut', 2)]
+        self.tuotteet = [Tuote('vuustonaxut', 2), Tuote('glögitiiviste', 4)]
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
@@ -18,3 +18,8 @@ class TestOstoskori(unittest.TestCase):
     def test_korissa_yksi_tuote_hinta_oikein(self):
         self.kori.lisaa_tuote(self.tuotteet[0])
         self.assertEqual(self.kori.hinta(), 2)
+
+    def test_korissa_kaksi_tavaraa_kahden_tuotteen_lisaamisen_jalkeen(self):
+        self.kori.lisaa_tuote(self.tuotteet[0])
+        self.kori.lisaa_tuote(self.tuotteet[1])
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
