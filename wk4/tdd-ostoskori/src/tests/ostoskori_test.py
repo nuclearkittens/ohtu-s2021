51cf1129257f28572acb2f8f5e77a49fa6e12c9d
meri saynatkari <meri.saynatkari@gmail.com>
2021-11-29 11:09:23 +0200

    wk4 ex5 pt5: test written (kahden tuotteen summa ok) and passed

--- wk4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ wk4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -23,3 +23,8 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.tuotteet[0])
         self.kori.lisaa_tuote(self.tuotteet[1])
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_korissa_kaksi_tavaraa_hinta_oikein(self):
+        self.kori.lisaa_tuote(self.tuotteet[0])
+        self.kori.lisaa_tuote(self.tuotteet[1])
+        self.assertEqual(self.kori.hinta(), 6)
