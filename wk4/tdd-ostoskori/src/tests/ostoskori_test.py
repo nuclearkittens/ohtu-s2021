ef5bf65af0237b275c5fe918e602c6eb0473b48d
meri saynatkari <meri.saynatkari@gmail.com>
2021-11-29 10:08:41 +0200

    wk4 ex5 pt2: test written

--- wk4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ wk4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -9,3 +9,7 @@ class TestOstoskori(unittest.TestCase):
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
+
+    def test_korissa_yksi_tuote_maara_oikein(self):
+        self.kori.lisaa_tuote(Tuote('vuustonaxut', 2))
+        self.assertEqual(self.kori.tavaroita_korissa(), 1)
